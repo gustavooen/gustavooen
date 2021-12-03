454b379172e9f7d4088418b34aacea2ada4a7c40
Gustavo Castro <gustavooenderman@gmail.com>
2021-12-03 16:32:11 -0300

    Primeiro commit

---
+++ javas.js
@@ -0,0 +1,204 @@
+/*FUNÇÕES de comprar, todas as funções são iguais, com
+descrições diferentes apenas*/
+//usando alert para comprar
+
+function compra1(){
+    var x;
+    var email=prompt("Digite seu e-mail:");
+    var senha=prompt("Digite sua senha:");
+    var tipo=prompt("Classe ECONOMICA [1] ou EXECUTIVA [2]")
+    if (tipo == 1){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$1.240,10 | CLASSE: ECONOMICA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoE());
+    }else if (tipo == 2){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$3.050,40 | CLASSE: EXECUTIVA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoV());
+    }
+}
+function compra2(){
+    var x;
+    var email=prompt("Digite seu e-mail:");
+    var senha=prompt("Digite sua senha:");
+    var tipo=prompt("Classe ECONOMICA [1] ou EXECUTIVA [2]")
+    if (tipo == 1){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$1.740,90 | CLASSE: ECONOMICA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoE());
+    }else if (tipo == 2){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$3.940,40 | CLASSE: EXECUTIVA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoV());
+    }
+}
+function compra3(){
+    var x;
+    var email=prompt("Digite seu e-mail:");
+    var senha=prompt("Digite sua senha:");
+    var tipo=prompt("Classe ECONOMICA [1] ou EXECUTIVA [2]")
+    if (tipo == 1){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$7.354,75 | CLASSE: ECONOMICA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoE());
+    }else if (tipo == 2){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$11.945,20 | CLASSE: EXECUTIVA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoV());
+    }
+}
+function compra4(){
+    var x;
+    var email=prompt("Digite seu e-mail:");
+    var senha=prompt("Digite sua senha:");
+    var tipo=prompt("Classe ECONOMICA [1] ou EXECUTIVA [2]")
+    if (tipo == 1){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$9.054,30 | CLASSE: ECONOMICA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoE());
+    }else if (tipo == 2){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$14.321,23 | CLASSE: EXECUTIVA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoV());
+    }
+}
+function compra5(){
+    var x;
+    var email=prompt("Digite seu e-mail:");
+    var senha=prompt("Digite sua senha:");
+    var tipo=prompt("Classe ECONOMICA [1] ou EXECUTIVA [2]")
+    if (tipo == 1){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$340,40 | CLASSE: ECONOMICA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoE());
+    }else if (tipo == 2){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$520,20 | CLASSE: EXECUTIVA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoV());
+    }
+}
+function compra6(){
+    var x;
+    var email=prompt("Digite seu e-mail:");
+    var senha=prompt("Digite sua senha:");
+    var tipo=prompt("Classe ECONOMICA [1] ou EXECUTIVA [2]")
+    if (tipo == 1){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$431,97 | CLASSE: ECONOMICA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoE());
+    }else if (tipo == 2){
+        var nome=prompt("Digite o primeiro nome do titular:");
+        var sobrenome=prompt("Digite o segundo nome do titular:");
+        var nc=prompt("Digite o número do cartão:");
+        var ns=prompt("Digite o código de segurança do cartão:");
+        var data=prompt("Digite a data de expiração do cartão:")
+        var r=confirm("Você CONFIRMA a COMPRA da passagem? PREÇO: R$710,23 | CLASSE: EXECUTIVA");
+        var r=confirm("Compra realizada com sucesso! O seu assento é: " + assentoV());
+    }
+}
+
+
+//funções de informações, usando apenas alert também
+
+function detalhes1(){
+    var x;
+    var r=confirm("Horário de partida: *19:15 - chegada: *21:20 | Preço ECONOMICA: R$1.240,10, EXECUTIVA: R$3.050,40 | *horário de BRASÍLIA");
+}
+function detalhes2(){
+    var x;
+    var r=confirm("Horário de partida: *21:10 - chegada: *01:05 | Preço ECONOMICA: R$1.740,90, EXECUTIVA: R$3.940,40 | *horário de BRASÍLIA");
+}
+function detalhes3(){
+    var x;
+    var r=confirm("Horário de partida: *18:00 - chegada: *09:30 | Preço ECONOMICA: R$7.354,75, EXECUTIVA: R$11.945,20 | *horário de BRASÍLIA");
+}
+function detalhes4(){
+    var x;
+    var r=confirm("Horário de partida: *17:30 - chegada: *08:45 | Preço ECONOMICA: R$9.054,30, EXECUTIVA: R$14.321,23 | *horário de BRASÍLIA");
+}
+function detalhes5(){
+    var x;
+    var r=confirm("Horário de partida: *08:45 - chegada: *10:00 | Preço ECONOMICA: R$340,40, EXECUTIVA: R$520,20 | *horário de BRASÍLIA");
+}
+function detalhes6(){
+    var x;
+    var r=confirm("Horário de partida: *11:30 - chegada: *14:00 | Preço ECONOMICA: R$431,97, EXECUTIVA: R$710,23 | *horário de BRASÍLIA");
+}
+
+
+//funções de entrada
+//email e telefone fictícios
+
+function cadastrar(){
+    var cadastrar=prompt("Digite um e-mail:");
+    var cadastrar2=prompt("Digite uma senha:");
+    var cadastrar3=prompt("Digite novamente a senha:");
+    var r=confirm("Cadastrado com sucesso!");
+}
+function contato(){
+    var r=confirm("E-mail: contato@rabyairlines.com | telefone: 0800 3785 4738");
+}
+function sobre(){
+    var r=confirm("Somos uma empresa aérea fundada em 2021, com sede em florianópolis, e com voos diretos de Florianópolis para o mundo.");
+}
+
+//função de gerar assento
+
+function assentoE(min, max){
+    min = Math.ceil(15);
+    max = Math.floor(62);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+function assentoV(min, max){
+    min = Math.ceil(1);
+    max = Math.floor(14);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+
